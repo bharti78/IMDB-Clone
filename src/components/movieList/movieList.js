617a47bf70df24d71b9eca50b9bc914de726c225
Bharti Dhote <152138758+bharti78@users.noreply.github.com>
2026-07-05 12:31:14 +0530

    Refactor getData function inside useEffect
    Refactored getData function to be defined within useEffect for better readability and to avoid redundancy.

--- src/components/movieList/movieList.js
+++ src/components/movieList/movieList.js
@@ -8,25 +8,28 @@ const MovieList = () => {
   const { type } = useParams();
 
   useEffect(() => {
+    const getData = () => {
+      fetch(
+        `https://api.themoviedb.org/3/movie/${
+          type ? type : "popular"
+        }?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`
+      )
+        .then((res) => res.json())
+        .then((data) => setMovieList(data.results));
+    };
+
     getData();
   }, [type]);
 
-  const getData = () => {
-    fetch(
-      `https://api.themoviedb.org/3/movie/${
-        type ? type : "popular"
-      }?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`
-    )
-      .then((res) => res.json())
-      .then((data) => setMovieList(data.results));
-  };
-
   return (
     <div className="movie__list">
-      <h2 className="list__title">{(type ? type : "POPULAR").toUpperCase()}</h2>
+      <h2 className="list__title">
+        {(type ? type : "POPULAR").toUpperCase()}
+      </h2>
+
       <div className="list__cards">
         {movieList.map((movie) => (
-          <Cards movie={movie} />
+          <Cards key={movie.id} movie={movie} />
         ))}
       </div>
     </div>
